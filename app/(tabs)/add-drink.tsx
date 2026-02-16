2c68b2c7e483f2b857973e110f8daa050cc20994
Anastasiia <a.v.kirianova@gmail.com>
2026-02-16 01:58:49 -0700

    wip

--- app/(tabs)/add-drink.tsx
+++ app/(tabs)/add-drink.tsx
@@ -20,17 +20,6 @@ export default function AddNewDrink() {
   return (
     <SafeAreaView style={{ flex: 1 }}>
       <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-        {/* <Slider
-          style={styles.slider}
-          minimumValue={0}
-          maximumValue={2000}
-          minimumTrackTintColor="#FF4B4B"
-          maximumTrackTintColor="#1A1A1A"
-          thumbImage={{
-            uri: 'https://png.pngtree.com/png-vector/20230318/ourmid/pngtree-duck-poultry-animal-transparent-on-white-background-png-image_6653170.png'
-          }}
-          vertical={true}
-        /> */}
 
         <CustomSlider
           onValueChange={(val) => setAmount(val)}
